bcd5a35d23e265c610a4c1942fc2c352b0770031
Babalu kumar <babalubt18mec011@gmail.com>
2023-08-19 13:19:02 +0530

    modal desiging

--- chaperone/src/main/java/chaperone/com/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package chaperone.com.repository;
-
-import chaperone.com.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long> {
-}
--- chaperone/src/main/java/chaperone/com/serviceImpl/AddressServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package chaperone.com.serviceImpl;
-
-import chaperone.com.service.AddressService;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class AddressServiceImpl implements AddressService {
-}
