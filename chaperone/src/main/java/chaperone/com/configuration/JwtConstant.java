f88bef91262dea06dfac4b6514ddd6326a715953
Babalu kumar <babalubt18mec011@gmail.com>
2023-08-19 16:19:15 +0530

    model

--- chaperone/src/main/java/chaperone/com/configuration/JwtConstant.java
+++
@@ -1,6 +0,0 @@
-package chaperone.com.configuration;
-
-public class JwtConstant {
-    public static final String SECRET_KEY="whvfuywvficweivbreigfuidbviufiywevyucvewiuviu";
-    public static final String JWT_HEADER="Authorization";
-}
---
+++ chaperone/src/main/java/chaperone/com/model/User.java
@@ -0,0 +1,26 @@
+package chaperone.com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+}
