e885bee8c3614302d5a5f6f1a383478cf34e966d
quarnstric <nishusainialwar@gmail.com>
2023-08-26 11:51:21 +0530

    CORS modified

--- chaperone/src/main/java/chaperone/com/securityConfig/MyConfig.java
+++ chaperone/src/main/java/chaperone/com/securityConfig/MyConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -69,16 +71,5 @@ public class MyConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement();
 
 
-    }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
